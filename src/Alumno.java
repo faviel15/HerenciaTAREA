53cc1276932e19431845c23bfba84355f9dfa264
faviel15 <favielgk@gmail.com>
2021-06-25 15:58:39 -0500

    subiendo

---
+++ src/Alumno.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Erick Faviel
+ */
+public class Alumno extends Persona{
+    private String numeroCuenta;
+    private String carrera;
+    private int semestre;
+
+    public Alumno(String nombre, int edad, int genero, float altura) {
+        super(nombre, edad, genero, altura);
+        
+    }
+
+    public Alumno(String numeroCuenta, String carrera, int semestre) {
+        this.numeroCuenta = numeroCuenta;
+        this.carrera = carrera;
+        this.semestre = semestre;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" + "numeroCuenta=" + numeroCuenta + ", carrera=" + carrera + ", semestre=" + semestre + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
